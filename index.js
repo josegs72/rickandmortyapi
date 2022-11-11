7d32a54c36767eac2461a05c25ee134f509ac26d
josegs72 <josegs72@gmail.com>
2022-11-11 11:31:42 +0100

    Feat 5

--- index.js
+++ index.js
@@ -23,51 +23,66 @@ const buscar = (event) => {
         <p class="species1">${personaje.species}</p>
         <p class="status1">${personaje.status}</p>
         <img class = "image" src="${personaje.image}" alt="${personaje.name}" />
-    </div>
-       
-          
-        `;
+    </div>  `;
         book$$.appendChild(card);
-      });
+      
+//RESETEO FORMULARIO//
+        formulario$$.reset();
+
+      
+ });
     });
 };
 
-formulario$$.addEventListener("submit", buscar);
+ formulario$$.addEventListener("submit", buscar);
 
-//personajes
 
-const pers = (characters) => {
+
+
+
+
+
+
+
+//personajes//
+
+function pers(characters) {
   console.log("MIS PERSONAJES", characters);
   for (const character of characters) {
     //ELEMENTOS//
     const character$$ = document.createElement("div");
     const names$$ = document.createElement("h3");
     const image$$ = document.createElement("img");
-    const species$$ = document.createElement("p");
+    //const species$$ = document.createElement("p");
 
     //CLASES//
-
     character$$.classList.add("character");
     names$$.classList.add("name");
     image$$.classList.add("image");
-    species$$.classList.add("species");
+    //species$$.classList.add("species");
 
     //CONTENIDO ELEMENTOS
-
     names$$.textContent = character.name;
     image$$.style.backgroundImage = `url(${character.image})`;
-    species$$.textContent = character.species;
+    //species$$.textContent = character.species;
 
     //INSERTO ELEMENTOS//
     character$$.appendChild(names$$);
-    character$$.appendChild(species$$);
+    //character$$.appendChild(species$$);
     character$$.appendChild(image$$);
     book$$.appendChild(character$$);
+
+
   }
 };
 
 fetch(
-  "https://rickandmortyapi.com/api/character/1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18,21,22,23"
+  "https://rickandmortyapi.com/api/character/1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18,21,22,23,24,25,26,27"
 )
   .then((response) => response.json())
   .then((characters) => pers(characters));
+
+
+
+
+
